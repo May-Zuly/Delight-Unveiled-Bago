76396839f543cc7b0be7571c57941a5b6f459eac
NaingAungLwin25 <nainglwin841@gmail.com>
2024-07-31 21:18:12 +0630

    checkout page

--- user/src/pages/CheckoutPage/index.jsx
+++ user/src/pages/CheckoutPage/index.jsx
@@ -1,4 +1,4 @@
-import { Card, Form, Input, Button, Row, Col, List, Typography } from "antd";
+import { Card, Form, Input, Button, Row, Col, List } from "antd";
 import { useRecoilValue } from "recoil";
 import { cart } from "../../store";
 import "./CheckoutPage.css";
@@ -18,10 +18,15 @@ const CheckoutPage = () => {
     );
   };
 
+  const totalPrice = cartData.reduce(
+    (total, item) => total + item.attributes.price * item.quantity,
+    0
+  );
+
   return (
     <div className="checkout-container">
-      <Row gutter={16}>
-        <Col xs={24} sm={24} md={12}>
+      <Row>
+        <Col xs={24} sm={24} md={14}>
           <Card title="Payment Details" className="checkout-form">
             <Form name="payment" layout="vertical" onFinish={onFinish}>
               <Form.Item
@@ -64,33 +69,52 @@ const CheckoutPage = () => {
 
               <Form.Item>
                 <Button type="primary" htmlType="submit" block>
-                  Pay ${getTotalAmount().toFixed(2)}
+                  Pay {getTotalAmount().toFixed(2)} MMK
                 </Button>
               </Form.Item>
             </Form>
           </Card>
         </Col>
-
-        <Col xs={24} sm={24} md={12}>
+        <Col xs={24} sm={24} md={8}>
           <Card title="Order Summary" className="summary-card">
-            <List
-              itemLayout="horizontal"
-              dataSource={cartData}
-              renderItem={(item) => (
-                <List.Item>
-                  <List.Item.Meta
-                    title={item.attributes.name}
-                    description={`Quantity: ${item.quantity}`}
-                  />
-                  <Typography.Text>
-                    ${(item.attributes.price * item.quantity).toFixed(2)}
-                  </Typography.Text>
-                </List.Item>
-              )}
-            />
-            <div className="total-amount">
-              Total: ${getTotalAmount().toFixed(2)}
-            </div>
+            <Row>
+              <Col xs={10} sm={10} md={10} lg={10}>
+                Item Name
+              </Col>
+              <Col xs={4} sm={4} md={4} lg={4}>
+                Quantity
+              </Col>
+              <Col xs={8} sm={8} md={8} lg={8}>
+                Subtotal
+              </Col>
+            </Row>
+            {
+              <List
+                itemLayout="horizontal"
+                dataSource={cartData}
+                renderItem={(item) => (
+                  <Row>
+                    <Col xs={10} sm={10} md={10} lg={10}>
+                      {item.attributes.name}
+                    </Col>
+                    <Col xs={4} sm={4} md={4} lg={4}>
+                      {item.quantity}
+                    </Col>
+                    <Col xs={8} sm={8} md={8} lg={8}>
+                      <div>{item.attributes.price * item.quantity} MMK</div>
+                    </Col>
+                  </Row>
+                )}
+              />
+            }
+            <Row style={{ padding: "10px 0", fontWeight: "bold" }}>
+              <Col xs={14} sm={14} md={14} lg={14}>
+                Total Amount:
+              </Col>
+              <Col xs={8} sm={8} md={8} lg={8}>
+                {totalPrice} MMK
+              </Col>
+            </Row>
           </Card>
         </Col>
       </Row>
