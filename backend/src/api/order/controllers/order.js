d7b3f0166d2175a8780b0ab6aefae25f50785b57
NaingAungLwin25 <nainglwin841@gmail.com>
2024-08-05 22:53:48 +0630

    add api and image

--- backend/src/api/order/controllers/order.js
+++ backend/src/api/order/controllers/order.js
@@ -7,9 +7,103 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async orderList(ctx) {
+    try {
+      const query = ctx.request.query;
+      const userId = query.user_id;
+      const fromDate = query.from;
+      const toDate = query.to;
+
+      const conditions = {};
+      if (userId) {
+        conditions.customer = { id: userId };
+      }
+      if (fromDate && toDate) {
+        conditions.createdAt = {
+          $gt: new Date(fromDate).toISOString(),
+          $lt: new Date(toDate).toISOString(),
+        };
+      } else if (fromDate) {
+        conditions.createdAt = {
+          $gt: new Date(fromDate).toISOString(),
+        };
+      } else if (toDate) {
+        conditions.createdAt = {
+          $lt: new Date(toDate).toISOString(),
+        };
+      }
+      const entries = await strapi.db.query("api::order.order").findMany({
+        orderBy: { createdAt: "desc" },
+        where: conditions,
+        populate: {
+          payment: true,
+          customer: true,
+          purchases: { populate: { product: { populate: { image: true } } } },
+        },
+      });
+      return entries.map((entry) => ({
+        ...entry,
+        payment: entry?.payment?.type,
+        customer: entry?.customer?.username,
+        purchases:
+          entry?.purchases.length > 0
+            ? entry?.purchases.map((p) => ({
+                product_name: p.product?.name,
+                product_description: p.product?.description,
+                product_image: p.product?.image?.url,
+                product_price: p.product?.price,
+                quantity: p?.quantity,
+              }))
+            : [],
+      }));
+    } catch (error) {
+      console.error(error);
+      ctx.badRequest(error, "Error in fetch order list");
+    }
+  },
+
+  async orderDetail(ctx) {
+    try {
+      const id = ctx.request.params.id;
+
+      if (!id) {
+        ctx.badRequest("ID is required");
+      }
+
+      const entry = await strapi.db.query("api::order.order").findOne({
+        where: { id },
+        populate: {
+          payment: true,
+          customer: true,
+          purchases: { populate: { product: { populate: { image: true } } } },
+        },
+      });
+
+      entry.payment = entry?.payment?.type;
+      entry.customer = entry?.customer?.username;
+      entry.purchases =
+        entry.purchases.length > 0
+          ? entry.purchases.map((p) => ({
+              product_name: p.product?.name,
+              product_description: p.product?.description,
+              product_image: p.product?.image?.url,
+              product_price: p.product?.price,
+              quantity: p?.quantity,
+            }))
+          : [];
+
+      return entry;
+    } catch (error) {
+      console.error(error);
+      ctx.badRequest(error, "Error in fetch order detail");
+    }
+  },
+
   async applyOrder(ctx) {
     try {
       const body = ctx.request.body;
+      //console.log(body);
+      //console.log(ctx.request.files);
       const promisePurchase = [];
       for (let i = 0; i < body.products.length; i++) {
         promisePurchase.push(
--- backend/src/api/order/routes/_custom.js
+++ backend/src/api/order/routes/_custom.js
@@ -1,6 +1,16 @@
 module.exports = {
   routes: [
     {
+      method: "GET",
+      path: "/order/list",
+      handler: "order.orderList",
+    },
+    {
+      method: "GET",
+      path: "/order/detail/:id",
+      handler: "order.orderDetail",
+    },
+    {
       method: "POST",
       path: "/order/apply",
       handler: "order.applyOrder",
